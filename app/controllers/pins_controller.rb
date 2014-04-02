a58f5675b7d0ec764d08cd9c6c456c60a4551eb0
Jonathan Rubenstein <jdrubens561@gmail.com>
2014-04-01 23:03:47 -0400

    authentication errors seem to be fixed

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -29,25 +29,20 @@ class PinsController < ApplicationController
 
     respond_to do |format|
       if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
+       format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        fotmat.html { render action: 'new' }
       end
     end
   end
-
   # PATCH/PUT /pins/1
   # PATCH/PUT /pins/1.json
   def update
     respond_to do |format|
       if @pin.update(pin_params)
         format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,7 +53,6 @@ class PinsController < ApplicationController
     @pin.destroy
     respond_to do |format|
       format.html { redirect_to pins_url }
-      format.json { head :no_content }
     end
   end
 
@@ -70,7 +64,7 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit pin" if @pin.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
     
     # Never trust parameters from the scary internet, only allow the white list through.
